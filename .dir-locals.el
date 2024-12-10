e4167ed39308b98c06818d5ca74453d8eea40feb
Serghei Iakovlev <git@serghei.pl>
2024-12-10 09:17:07 +0100

    Reformat documentation

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,8 +1,12 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((c-mode
-  (indent-tabs-mode . nil)   ;; Use spaces instead of tabs
-  (c-basic-offset . 2)       ;; Set indentation level to 2 spaces
-  (fill-column . 80)         ;; Maximum line length is 80 characters
-  (c-file-style . "gnu")))   ;; Use GNU coding style for C files
+((nil
+  (sentence-end-double-space . t))
+ (makefile-gmake-mode
+  (outline-regexp . "##"))
+ (c-mode
+  (indent-tabs-mode . nil)
+  (c-basic-offset . 2)
+  (fill-column . 80)
+  (c-file-style . "gnu")))
