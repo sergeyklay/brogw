9af9c86b6f97d91787d624b77f5fe5f9793e4ad3
Serghei Iakovlev <git@serghei.pl>
2024-12-15 11:05:10 +0100

    Update directory local variables

--- .dir-locals.el
+++ .dir-locals.el
@@ -18,12 +18,10 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil
-  (sentence-end-double-space . t))
- (makefile-gmake-mode
-  (outline-regexp . "##"))
- (c-mode
-  (indent-tabs-mode . nil)
-  (c-basic-offset . 2)
-  (fill-column . 80)
-  (c-file-style . "gnu")))
+((nil . ((sentence-end-double-space . t)))
+ (sh-mode . ((sh-basic-offset . 2)))
+ (makefile-gmake-mode . ((outline-regexp . "#\\(#+\\)")))
+ (c-mode . ((indent-tabs-mode . nil)
+            (c-basic-offset . 2)
+            (fill-column . 80)
+            (c-file-style . "gnu"))))
